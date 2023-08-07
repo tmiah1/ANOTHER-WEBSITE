2a2dedbcea3214fa61348a160a1fdda90be6efc4
ahmed <ahmedtalha.miah@gmail.com>
2023-08-07 15:17:54 +0100

    initialized git repository

--- index (3).js
+++ index (3).js
@@ -1,2 +1,24 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+async function main() {
+    const users = await fetch("https://jsonplaceholder.typicode.com/users") 
+    const usersData = await users.json();
+    const userListEl = document.querySelector(".user-list");
+    console.log(usersData);
+    userListEl.innerHTML = usersData
+    .map (
+        (user) => 
+         `<div class="user-card">
+            <div class="user-card__container">
+              <h3>User's Name is here</h4>
+                <p><b>Email:</b> email@email.com</p>
+                <p><b>Phone:</b> 0000000000</p>
+                <p><b>Website:</b> <a href="https://website.website" target="_blank">website.website</a></p>
+            </div>
+          </div>`
+    )
+    .join("");
+}
+
+main();
